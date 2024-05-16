b998b521059a64fca33bad279ca7bdd82e8fcaeb
Ah3laly <Ah3laly@gmail.com>
2024-05-16 16:32:34 +0800

    Implement StoreController.create method

--- src/main/java/com/neurogine/storeapi/controllers/StoreController.java
+++ src/main/java/com/neurogine/storeapi/controllers/StoreController.java
@@ -1,12 +1,38 @@
 package com.neurogine.storeapi.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.neurogine.storeapi.beans.ApiResponse;
+import com.neurogine.storeapi.configurations.StoreMessagingConfiguration;
+import com.neurogine.storeapi.entities.Store;
 
 @RestController
 @RequestMapping("/api/store")
 public class StoreController {
-	public void create() {};
+	
+	@Autowired
+	StoreMessagingConfiguration.StoreService storeService;
+	
+	@PostMapping
+	public ResponseEntity<ApiResponse<?>> create(
+		HttpServletRequest request,
+		@RequestBody Store store) {
+		try {
+			Store result = storeService.create(store);
+			return new ResponseEntity<>(new ApiResponse<Store>(ApiResponse.STATUS.OK, "CREATED", result), HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(new ApiResponse<String>(ApiResponse.STATUS.ERROR, e.getMessage(), ""), HttpStatus.BAD_REQUEST);
+		}
+	}
+	
 	public void update() {};
 	public void get() {};
 	public void delete() {};
