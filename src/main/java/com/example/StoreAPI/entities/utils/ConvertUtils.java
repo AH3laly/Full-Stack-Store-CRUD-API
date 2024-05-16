f20c53c357116873f987f7ae3d9f2eacc311ac73
Ah3laly <Ah3laly@gmail.com>
2024-05-16 15:59:18 +0800

    Create & Implement ConvertUtils method

---
+++ src/main/java/com/example/StoreAPI/entities/utils/ConvertUtils.java
@@ -0,0 +1,34 @@
+package com.example.StoreAPI.entities.utils;
+
+import java.util.Map;
+
+import org.bson.Document;
+
+import com.example.StoreAPI.entities.Store;
+
+public class ConvertUtils {
+	
+	public static Document ConvertStoreToDocument(Store store) {
+		Document document = new Document();
+		document.put("name", store.getName());
+        document.put("location", store.getLocation());
+        document.put("tags", store.getTags());
+        document.put("image", store.getImage());
+        document.put("promotions", store.getPromotions());
+        document.put("ratings", store.getRating());
+		return document;
+	}
+
+	public static Store ConvertDocumentToStore(Document document) {
+		Store store = new Store();
+		store.setId(document.getObjectId("_id").toString());
+		store.setName(document.getString("name"));
+		store.setImage(document.getString("image"));
+		store.setLocation((Map<String, Double>)document.get("location"));
+		store.setPromotions(document.getList("promotions", String.class));
+		store.setTags(document.getList("tags", String.class));
+		store.setRating(document.getDouble("ratings"));
+		return store;
+	}
+
+}
