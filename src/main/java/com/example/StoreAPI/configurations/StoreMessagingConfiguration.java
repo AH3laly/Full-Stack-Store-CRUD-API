03ba5f1d1aead27c3ce46bf7c8df6e084f08f030
Ah3laly <Ah3laly@gmail.com>
2024-05-16 15:41:32 +0800

    Create Messaging Gateway interface Skeleton

--- src/main/java/com/example/StoreAPI/configurations/StoreMessagingConfiguration.java
+++ src/main/java/com/example/StoreAPI/configurations/StoreMessagingConfiguration.java
@@ -1,5 +1,24 @@
 package com.example.StoreAPI.configurations;
 
-public class StoreMessagingConfiguration {
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
 
+public class StoreMessagingConfiguration {
+	@MessagingGateway
+    public interface StoreService {
+
+        
+        
+        @Gateway(requestChannel = "createStore.input")
+        void create();
+        
+        @Gateway(requestChannel = "updateStore.input")
+        void update();
+        
+        @Gateway(requestChannel = "loadStore.input")
+        void load();
+        
+        @Gateway(requestChannel = "deleteStore.input")
+        void delete();
+    }
 }
