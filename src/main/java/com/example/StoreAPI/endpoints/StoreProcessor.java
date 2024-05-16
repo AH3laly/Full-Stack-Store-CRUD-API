95e79ed80b4c7f03452f39ac77c3309a98143a29
Ah3laly <Ah3laly@gmail.com>
2024-05-16 15:50:06 +0800

    Create StoreProcessor endpoint

---
+++ src/main/java/com/example/StoreAPI/endpoints/StoreProcessor.java
@@ -0,0 +1,23 @@
+package com.example.StoreAPI.endpoints;
+
+import com.example.StoreAPI.entities.Store;
+
+public class StoreProcessor {
+
+	public Store load(Store store) {
+		return store;
+    }
+	
+	public Store create(Store store) {
+		return store;
+    }
+
+	public Store update(Store store) {
+		return store;
+    }
+
+	public Store delete(Store store) {
+		return store;
+    }
+
+}
