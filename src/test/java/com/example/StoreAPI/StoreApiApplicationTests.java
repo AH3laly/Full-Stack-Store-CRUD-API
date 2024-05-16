37526bc85921cc336f20894fbae7005416080302
Ah3laly <Ah3laly@gmail.com>
2024-05-16 15:18:02 +0800

    Initial Spring Boot Project

---
+++ src/test/java/com/example/StoreAPI/StoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StoreAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
