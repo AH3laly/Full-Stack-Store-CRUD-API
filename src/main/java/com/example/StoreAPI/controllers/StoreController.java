31e65687ffc3d520d8569f3beb2ccaaed8ce700b
Ah3laly <Ah3laly@gmail.com>
2024-05-16 15:42:40 +0800

    Create StoreController Skeleton methods

--- src/main/java/com/example/StoreAPI/controllers/StoreController.java
+++ src/main/java/com/example/StoreAPI/controllers/StoreController.java
@@ -1,5 +1,13 @@
 package com.example.StoreAPI.controllers;
 
-public class StoreController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/store")
+public class StoreController {
+	public void create() {};
+	public void update() {};
+	public void get() {};
+	public void delete() {};
 }
