9dd087d843df490fb68c22ec6ec690ac98661bef
Ah3laly <Ah3laly@gmail.com>
2024-05-16 15:48:47 +0800

    Implement Store entity

--- src/main/java/com/example/StoreAPI/entities/Store.java
+++ src/main/java/com/example/StoreAPI/entities/Store.java
@@ -1,5 +1,72 @@
 package com.example.StoreAPI.entities;
 
+import java.util.List;
+import java.util.Map;
+
 public class Store {
 
+	private String id;
+	private String image;
+	private String name;
+	private Map<String, Double> location;
+	
+	private List<String> tags;
+	private List<String> promotions;
+	private Double rating;
+
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+    public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Map<String, Double> getLocation() {
+		return location;
+	}
+
+	public void setLocation(Map<String, Double> location) {
+		this.location = location;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	public List<String> getPromotions() {
+		return promotions;
+	}
+
+	public void setPromotions(List<String> promotions) {
+		this.promotions = promotions;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
 }
